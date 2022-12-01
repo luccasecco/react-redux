bb20a5de7bc41456cbcef5592d9d44b279a7824b
luccasecco <luccasecco@hotmail.com>
2022-12-01 20:36:56 -0300

    initial commit

---
+++ src/components/Catalog.tsx
@@ -0,0 +1,44 @@
+import { useCallback, useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+
+import { IProduct } from "../store/modules/cart/types"
+import api from "../services/api"
+import { addProductToCart } from "../store/modules/cart/actions"
+
+const Catalog: React.FC = () => {
+  const dispatch = useDispatch()
+  const [catalog, setCatalog] = useState<IProduct[]>([])
+
+  useEffect(()=> {
+    api.get('products').then(response => {
+      setCatalog(response.data)
+    })
+  }, [])
+
+ const handleAddProductToCart = useCallback((product: IProduct)=> {
+  dispatch(addProductToCart(product))
+ }, [dispatch])
+
+  return (
+    <main >
+      <h1>Catalog</h1>
+  
+      {catalog.map(product => (
+        <article key={product.id} style={{display: 'flex', gap: 6}}>
+          <strong>{product.title}</strong>{" - "}
+          <span>{product.price}</span>{"  "}
+
+          <button 
+            type="button"
+            onClick={()=> handleAddProductToCart(product)}
+          >
+            Comprar
+          </button>
+        </article>
+      ))}
+
+    </main>
+  )
+}
+
+export default Catalog
